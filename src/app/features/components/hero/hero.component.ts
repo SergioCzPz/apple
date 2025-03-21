51095ba8587c3f9eeab8a3e9e4b11ce251cae021
Sergio Cruz <sergioscp@protonmail.com>
2025-03-20 20:40:58 -0600

    hero built

--- src/app/features/components/hero/hero.component.ts
+++ src/app/features/components/hero/hero.component.ts
@@ -1,4 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import gsap from 'gsap';
+import { Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-hero',
@@ -11,4 +26,45 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeroComponent {}
+export class HeroComponent implements AfterViewInit, OnInit, OnDestroy {
+  private readonly breakpointObserver = inject(BreakpointObserver);
+  private readonly platformId = inject(PLATFORM_ID);
+
+  private readonly isSmallScreen: WritableSignal<boolean> = signal(false);
+  public videoSrc = computed(() =>
+    this.isSmallScreen()
+      ? 'assets/videos/smallHero.mp4'
+      : 'assets/videos/hero.mp4'
+  );
+  private destroyed = new Subject<void>();
+
+  ngOnInit(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    this.breakpointObserver
+      .observe(['(max-width: 759px)'])
+      .pipe(takeUntil(this.destroyed))
+      .pipe(tap(console.log))
+      .subscribe(result => this.isSmallScreen.set(result.matches));
+  }
+
+  ngAfterViewInit(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    gsap.to('#hero', {
+      opacity: 1,
+      delay: 2,
+    });
+
+    gsap.to('#cta', {
+      opacity: 1,
+      translateY: '-80px',
+      delay: 2,
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
+}
