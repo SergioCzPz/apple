e9bc39cbf3ac0148149de04eada5e97ccfe43256
Sergio Cruz <sergioscp@protonmail.com>
2025-03-20 22:34:24 -0600

    highlights built

--- src/app/features/components/highlights/highlights.component.ts
+++ src/app/features/components/highlights/highlights.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  PLATFORM_ID,
+} from '@angular/core';
+
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-highlights',
@@ -11,4 +20,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HighlightsComponent {}
+export class HighlightsComponent implements AfterViewInit {
+  private readonly platformId = inject(PLATFORM_ID);
+
+  ngAfterViewInit(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    gsap.to('#title', {
+      opacity: 1,
+      y: 0,
+    });
+
+    gsap.to('.link', {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
+    });
+  }
+}
